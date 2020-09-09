1810fa9e342c04305e914557c9ce8d9e686b220a
Rebekah Salem-Dalgety <rebekahsalem-dalgety@Rebekahs-MacBook-Pro.local>
2020-09-09 11:50:12 +0100

    6 passing test

--- test/drink_test.py
+++ test/drink_test.py
@@ -5,12 +5,11 @@ from src.drink import Drink
 
 class TestDrink(unittest.TestCase):
     def setUp(self):
-        drink_1 = Drink("Elven Wine", 9.99)
-        drink_2 = Drink("Dwarf Ale", 10.99)
-        self.drink = [drink_1, drink_2]
+        self.drink = Drink("Elven Wine", 9.99)
+        
     
-    # def test_drink_has_name(self):
-    #     self.assertEqual("Elven Wine", self.drink.name)
+    def test_drink_has_name(self):
+        self.assertEqual("Elven Wine", self.drink.name)
 
-    # def test_drink_has_price(self):
-    #     self.assertEqual(9.99, self.drink.price)
+    def test_drink_has_price(self):
+        self.assertEqual(9.99, self.drink.price)
