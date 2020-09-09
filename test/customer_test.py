041978ac490dc813f1ed340761cb4b70d2d0dfd1
Rebekah Salem-Dalgety <rebekahsalem-dalgety@Rebekahs-MacBook-Pro.local>
2020-09-09 12:15:50 +0100

    8 passes

--- test/customer_test.py
+++ test/customer_test.py
@@ -9,6 +9,7 @@ class TestCustomer(unittest.TestCase):
     def setUp(self):
         self.drink = Drink("Elven Wine", 9.99)        
         self.customer = Customer("Aragorn", 100.00)
+        self.pub = Pub("The Prancing Pony", 100, self.drink)
 
     def test_customer_name(self):
         self.assertEqual("Aragorn", self.customer.name)
@@ -19,5 +20,12 @@ class TestCustomer(unittest.TestCase):
     def test_reduce_wallet(self):
         self.customer.reduce_wallet(self.drink)
         self.assertEqual(90.01, self.customer.wallet)
+    
+    def test_buy_drink(self):
+        self.customer.reduce_wallet(self.drink)
+        self.pub.increase_till(self.drink)
+        self.assertEqual(90.01, self.customer.wallet)
+        self.assertEqual(109.99, self.pub.till)
+
 
     
